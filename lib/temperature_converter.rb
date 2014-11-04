b7738b5f36c6c27d9c775b6e50ca8b3926f46848
postazure <gordonmaxc@gmail.com>
2014-11-04 10:26:04 -0800

    complete

--- lib/temperature_converter.rb
+++ lib/temperature_converter.rb
@@ -1,2 +1,52 @@
 class TemperatureConverter
+  def initialize unit, value
+    @unit = unit
+    @value = value
+  end
+
+  def unit
+    @unit
+  end
+
+  def value
+    @value
+  end
+
+  def to_celsius
+    if @unit == :celsius
+      converted_value  = @value
+    elsif @unit == :kelvin
+      converted_value = @value - 273.15
+    elsif @unit == :fahrenheit
+      converted_value = (@value - 32.0)* (5.0/9.0)
+
+    end
+
+    converted_value
+  end
+
+  def to_fahrenheit
+    if @unit == :fahrenheit
+      converted_value = @value
+    elsif @unit == :celsius
+      converted_value = @value *(9.0/5.0) +32.0
+    elsif @unit == :kelvin
+      converted_value = (@value- 273.15) *(9.0/5.0) +32.0
+
+    end
+
+    converted_value
+  end
+
+  def to_kelvin
+    if @unit == :kelvin
+      converted_value = @value
+    elsif @unit == :celsius
+      converted_value = @value + 273.15
+    elsif @unit == :fahrenheit
+      converted_value = (@value-32.0)/1.8 +273.15
+    end
+
+    converted_value
+  end
 end
